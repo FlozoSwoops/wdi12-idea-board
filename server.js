c415138adad6225c7da46a0086af4d053dee53bb
flozoswoops <flores.a.mark@gmail.com>
2017-10-12 11:44:24 -0400

    step three

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
-require("dotenv").config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const app = express();
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI)
+// Imports variables from .env file
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+mongoose.Promise = global.Promise
+// Create a new app using express
+const app = express()
 
-const connection = mongoose.connection;
+// Connect to MongoDB and set up messages for when 
+// Mongo connects successfully or errors out
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
+const connection = mongoose.connection
+
 connection.on('connected', () => {
-  console.log('Mongoose Connected Successfully');    
-}); 
-
-// If the connection throws an error
-connection.on('error', (err) => {
-  console.log('Mongoose default connection error: ' + err);
-}); 
-
-app.use(bodyParser.json());
-app.get('/', (req,res) => {
-  res.send('Hello world!')
+  console.log('Successfully connected to MongoDB')
 })
 
-const PORT = process.env.PORT || 3000;
+connection.on('error', (err) => {
+  console.log('MongoDB Error: ', err)
+})
+
+// Inject middleware
+app.use(bodyParser.json())
+app.use(express.static(`${__dirname}/client/build`))
+
+
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
+
+// Set the app to listen on a specific port
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log("Magic happening on port " + PORT);
+  console.log('App listening on port: ', PORT)
 })
